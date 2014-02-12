7d8c9acff933b3f2617848e3b8aed5d9da38ce58
streed <sean@cheapfit.me>
2014-02-11 21:37:32 -0500

    Simple RESTFUL API to accept a json representation of a invoice which will then be rendered to a PDF on the backend and the resulting invoice document will be stored to the server.

--- fetchBooks/app.py
+++ fetchBooks/app.py
@@ -1,3 +1,30 @@
-from flask import Flask
+import json
+from flask import Flask, request
+from flask.ext.restful import Api, Resource
+
+from .handler.render import FetchBooks as FB
 
 app = Flask( __name__ )
+api = Api( app )
+
+class FetchBooks( Resource ):
+
+    def post( self ):
+        invoice = request.json
+
+        invoice = FB.invoiceFromJson( invoice )
+
+        client = FB.makeClient()
+
+        ret = client.render_invoice( invoice )
+
+        print ret
+
+        return { "status": ret.status, "timestamp": ret.timestamp, "invoiceUrl": ret.invoiceUrl }
+
+api.add_resource( FetchBooks, "/invoice" )
+
+
+
+
+
